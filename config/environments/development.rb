e66c3b1d62f6a4c4c72ad82c833897acdae36c05
HamaguchiRyosuke <hamaguchi.ryosuke@mwed.co.jp>
2017-07-27 20:05:42 +0900

    Add a heroku set

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,13 +48,13 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :user_name => 'c37fde54b819db',
-    :password => '5ac48b11be0fb9',
-    :address => 'smtp.mailtrap.io',
-    :domain => 'smtp.mailtrap.io',
-    :port => '2525',
-    :authentication => :cram_md5
+  ActionMailer::Base.smtp_settings = {
+    :user_name => 'noreplyskiller@gmail.com',
+    :password => ENV["SENDGRID_PASSWORD"],
+    :address => 'smtp.gmail.com',
+    :domain => 'gmail.com',
+    :port => '587',
+    :authentication => :plain
   }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
